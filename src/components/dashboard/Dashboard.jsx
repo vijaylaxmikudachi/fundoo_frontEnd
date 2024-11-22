aa7f86d5f7b9c94a714cef9a33c146eae424af48
Vijaylaxmi <vijaylaxmikudachi80@gmail.com>
2024-11-22 12:40:42 +0530

    [vijaylaxmi]Added:Dashboard components:-Headder Navlist Swipebar.

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,48 +1,19 @@
-import React from "react";
-import { Link, Outlet } from "react-router-dom";
-import { Drawer, List, ListItem, ListItemIcon, ListItemText, Toolbar } from "@mui/material";
-import NoteIcon from "@mui/icons-material/Note";
-import LabelIcon from "@mui/icons-material/Label";
-import ArchiveIcon from "@mui/icons-material/Archive";
-import DeleteIcon from "@mui/icons-material/Delete";
+import { BrowserRouter as Router, Routes, Route, Outlet } from 'react-router-dom';
+import { Box } from '@mui/material';
 
-const drawerWidth = 240;
+//components
+import SwipeDrawer from './SwipeDrawer';
 
 const Dashboard = () => {
-  const menuItems = [
-    { text: "Notes", icon: <NoteIcon />, path: "/" },
-    { text: "Edit Labels", icon: <LabelIcon />, path: "/edit-labels" },
-    { text: "Archive", icon: <ArchiveIcon />, path: "/archive" },
-    { text: "Trash", icon: <DeleteIcon />, path: "/trash" },
-  ];
-
-  return (
-    <div style={{ display: "flex" }}>
-      {/* Side Drawer */}
-      <Drawer
-        variant="permanent"
-        sx={{
-          width: drawerWidth,
-          flexShrink: 0,
-          [`& .MuiDrawer-paper`]: { width: drawerWidth, boxSizing: "border-box" },
-        }}
-      >
-        <Toolbar />
-        <List>
-          {menuItems.map((item, index) => (
-            <ListItem button component={Link} to={item.path} key={index}>
-              <ListItemIcon>{item.icon}</ListItemIcon>
-              <ListItemText primary={item.text} />
-            </ListItem>
-          ))}
-        </List>
-      </Drawer>
-
-      <main style={{ flexGrow: 1, padding: "20px" }}>
-        <Outlet />
-      </main>
-    </div>
-  );
-};
+    return (
+        <Box style={{ display: 'flex', width: '100%' }}>
+          
+            <SwipeDrawer />
+            <Box style={{ flexGrow: 1, padding: '16px' }}>
+                <Outlet /> 
+            </Box>
+        </Box>
+    )
+}
 
 export default Dashboard;
